e8542f1dcb9a3a6859338e72c5698b4d79b113ce
Brennan Vincello <brennanv03@gmail.com>
2017-10-02 06:08:31 -0700

    clean up

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route("/hello")
 def hello():
+    return 'hello'
+
+@app.route("/health")
+def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
